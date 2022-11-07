2a98a09ce54677790098754186c71ad46275ddba
Cristian Bazan <johanwebdesigner@gmail.com>
2022-11-07 00:02:53 -0300

    "Adding php"

---
+++ home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Get Jinxed!</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="icon" href="D:\Cristian\Documents\Diseño Web\Practicas\GetJinxed\Icono.ico">
+
+	</head>
+	<body>
+	<div class="grid-container">
+		<header>
+			
+		</header>
+
+		<main>
+			<div class="container">
+				<h1 class="title">Get <br> Jinxed!</h1>
+			</div>
+			<div class="grid-gallery">
+				<img class="img" src="img/1.jpg" id="img-1">
+				<img class="img" src="img/2.jpg" id="img-2">
+				<img class="img" src="img/3.jpg" id="img-3">
+				<img class="img" src="img/4.jpg" id="img-4">
+				<img class="img" src="img/5.jpg" id="img-5">
+				<img class="img" src="img/6.jpg" id="img-6">
+				<img class="img" src="img/7.jpg" id="img-7">
+				<img class="img" src="img/8.jpg" id="img-8">
+				<img class="img" src="img/9.jpg" id="img-9">
+				<img class="img" src="img/10.jpg" id="img-10">
+				<img class="img" src="img/11.jpg" id="img-11">
+				<img class="img" src="img/12.jpg" id="img-12">
+				<img class="img" src="img/13.jpg" id="img-13">
+				<img class="img" src="img/14.jpg" id="img-14">
+				<img class="img" src="img/15.jpg" id="img-15">
+				<img class="img" src="img/16.jpg" id="img-16">
+				<img class="img" src="img/17.jpg" id="img-17">
+			</div>
+
+			<div class="modal-image">
+
+			</div>
+
+			<div class="modal">
+				
+			</div>
+		</main>
+
+		<footer>
+			
+		</footer>
+	</body>
+	<script src="./js/code.js"></script>
+	</body>
+</html>
+
+
